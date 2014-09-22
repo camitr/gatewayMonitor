78e5cf5cc883adea92885c2e49137fb551d40231
camitr <shriamit1@gmail.com>
2014-09-22 18:30:40 +0530

    average size and total packets calculation is added

--- Cmonitor.sh
+++ Cmonitor.sh
@@ -34,31 +34,42 @@ server=10.129.200.200
 echo "Duration of Test in Seconds"
 read dur
 
-#mkdir testof-$(date +%d%H%M)
+mkdir testof-$(date +%d%H%M)
 
-#cd testof-$(date +%d%H%M)
+cd testof-$(date +%d%H%M)
 
 tshark -i eth0 -a duration:$dur $para -w PacketCap.pcap
 
 i=0
-	for each in `cat 1.txt`; do
+	for each in `cat ../1.txt`; do
 		ip[i]=$each
-		tshark -Y "ip.src == ${ip[i]} && ip.dst == $server" -r PacketCap.pcap >ip$i-.csv
-		sed -i '/TCP Dup/d' ip$i-.csv
-		sed -i '/TCP Retransmission/d' ip$i-.csv
+		tshark -Y "ip.src == ${ip[i]} && ip.dst == $server" -r PacketCap.pcap >ClientUp$i-.csv
+		sed -i '/TCP Dup/d' ClientUp$i-.csv
+		sed -i '/TCP Retransmission/d' ClientUp$i-.csv
 
+
+		tshark -Y "ip.dst == ${ip[i]} && ip.src == $server" -r PacketCap.pcap >ClientDown$i-.csv
+		sed -i '/TCP Dup/d' ClientDown$i-.csv
+		sed -i '/TCP Retransmission/d' ClientDown$i-.csv
+
+## Total Packet Calculation for Upload and Download
+
+		awk 'END {print NR}' ClientUp$i-.csv>TotalClientUpPacket$i-.csv 
+		awk 'END {print NR}' ClientDown$i-.csv>TotalClientDownPacket$i-.csv 
+
+## Average packet per second transmited
+
+		awk '{avgPckt=$1/'$dur'} {print avgPckt}' TotalClientUpPacket$i-.csv>AvrgUpPacket$i-.csv
+		awk '{avgPckt=$1/'$dur'} {print avgPckt}' TotalClientDownPacket$i-.csv>AvrgDownPacket$i-.csv
+
+## Average packet size Upload and Download 
+
+		awk '{sum+=$7}  END { print "Average = ",sum/NR} {print Average}' ClientUp$i-.csv>AvrgUpSize$i-.csv 
+		awk '{sum+=$7}  END { print "Average = ",sum/NR} {print Average}' ClientDown$i-.csv>AvrgDownSize$i-.csv 
 		i=$(expr $i + 1)
 		
 	done
-#echo ${ip[1]}
-#
-#
-#
  
-#if	[ $num -eq 1 ]
-#then 	
-#	{
-#		awk 'END {print NR}' CapFileTestSrc.csv>TotalPacket.csv 
 #		echo " Total number of Packet Capture:" `cat TotalPacket.csv`
 #
 #		sed -n "/$server/p" CapFileTestSrc.csv > CapServerPackets.csv
